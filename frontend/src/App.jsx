f98d1fc2ae24d6f3a2d87db63fa1aa024e411278
tyranno <eotjdgkdl@gmail.com>
2024-06-11 17:03:33 +0900

    api 주소를 env파일에서 가져오도록 변경

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   const getHello = () => {
     const greet = document.getElementById("greet")
-    fetch('http://localhost:4000/api/hello')
+    fetch(`${import.meta.env.VITE_API_SERVER}/api/hello`)
     .then(res => res.json())
     .then(data => greet.innerHTML = JSON.stringify(data))
   }
